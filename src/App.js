494ec671d2ebcc19e53d25630df0d55809f81c98
Lemuella01 <danlemmy01@gmail.com>
2022-09-28 21:19:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Private_trips from './component/Private/Private_trips';
 
 function App() {
   return (
     <div className="App">
-      
+      <Private_trips/>
     </div>
   );
 }
---
+++ src/component/Private/Private_trips.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Private_trips = () => {
+  return (
+    <div>Private_trips</div>
+  )
+}
+
+export default Private_trips
